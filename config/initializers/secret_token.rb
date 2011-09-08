56e9e84757f22c4620198c2feb752f7aa30ac305
Todd Sedano <todd.sedano@sv.cmu.edu>
2011-09-08 12:20:51 -0400

    Initial repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HeavenFull::Application.config.secret_token = 'fde7db1ab5233ca2ce669527a7679c08b37c6167f60ba2d240704ea7caf10d22e570a2135e5f1c555cb3af44fd20cfd02005c2835dd352a395bae4a42b33e82b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+HeavenFull::Application.config.session_store :cookie_store, :key => '_heaven_full_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# HeavenFull::Application.config.session_store :active_record_store
